7351c6cd1dd14f568c2b6623966197879e03f744
WilliMu-1255 <2531742018@mail.utec.edu.sv>
2023-02-12 19:24:32 -0600

    inicial

--- ControlesLayout_08022023/app/src/main/java/sv/edu/utec/controleslayout_08_02_2023/MainActivity.java
+++ ControlesLayout_08022023/app/src/main/java/sv/edu/utec/controleslayout_08_02_2023/MainActivity.java
@@ -3,6 +3,7 @@ package sv.edu.utec.controleslayout_08_02_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -23,9 +24,34 @@ public class MainActivity extends AppCompatActivity {
         etInfo=findViewById(R.id.editTextTextMultiLine);
         spPais=findViewById(R.id.spnPais);
 
-        String[]opciones={"Guatemala","El Salvador","Honduras","Nicaragua","Costa Rica","Panamá"};
+        String[]opciones={"Seleccione un pais","Guatemala","El Salvador","Honduras","Nicaragua","Costa Rica","Panamá"};
         ArrayAdapter<String> adaptador= new ArrayAdapter<String>(this,
                 androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,opciones);
+        spPais.setAdapter(adaptador);
+
+    }
+
+    public void Almacenar(View v){
+        String datos="";
+        String seleccion = spPais.getSelectedItem().toString();
+        if (seleccion.equals("Seleccione un pais")){
+            datos="No selecciono ningun pais";
+            etInfo.append(datos);
+
+        }
+        else if (seleccion.equals("Guatemala")){
+                datos="Guatemala";
+            etInfo.append(datos);
+        }
+        else if (seleccion.equals("Guatemala")){
+            datos="El Salvador";
+            etInfo.append(datos);
+        }
+        else if (seleccion.equals("Guatemala")){
+            datos="Honduras";
+            etInfo.append(datos);
+        }
+
 
     }
 }
