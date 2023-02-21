ea43a23d7171391a46473851d21d1e9aa8b252b9
WilliMu-1255 <2531742018@mail.utec.edu.sv>
2023-02-20 19:45:15 -0600

    inicial

---
+++ Activity_Pantallas/app/src/main/java/sv/edu/utec/activity_pantallas/MainActivity.java
@@ -0,0 +1,33 @@
+package sv.edu.utec.activity_pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etNombre;
+    TextView twresultado;
+    Button btnEnv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etNombre = findViewById(R.id.edtNombre);
+        twresultado=findViewById(R.id.txvBienvenida);
+    }
+
+    public void Bienve(View v)
+    {
+        String nombre=etNombre.getText().toString();
+        twresultado.setText("Bienvenido: "+nombre);
+
+    }
+
+
+}
